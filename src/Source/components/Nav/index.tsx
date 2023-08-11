dad30946fd730b36b22d43b1af3a220acd2a85ec
Albert <abeiphone@bk.ru>
2023-08-11 15:36:25 +0300

    add nav buttons hover animation

--- src/Source/components/Nav/index.tsx
+++ src/Source/components/Nav/index.tsx
@@ -3,11 +3,11 @@ import { MenuBurger } from "./MenuBurger";
 import { Switcher } from "../Switcher/Switcher";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import "./styles.css";
+<div className="translate"></div>;
 
 export const Navbar: FC = () => {
   const isMobile = useMediaQuery("(max-width:767.98px)");
   const isHome = window.location.pathname === "/";
-  
   return (
     <>
       {!isMobile ? (
@@ -17,21 +17,25 @@ export const Navbar: FC = () => {
               {isHome ? (
                 <>
                   <li className="menu__item">
+                    <div className="translate" />
                     <a href="/apps" className="menu__link">
                       Mini-Apps
                     </a>
                   </li>
                   <li className="menu__item">
+                    <div className="translate" />
                     <a href="#about" className="menu__link">
                       ABOUT
                     </a>
                   </li>
                   <li className="menu__item">
+                    <div className="translate" />
                     <a href="#service" className="menu__link">
                       SERVICE
                     </a>
                   </li>
                   <li className="menu__item">
+                    <div className="translate" />
                     <a href="#experience" className="menu__link">
                       Work Experience
                     </a>
@@ -39,6 +43,7 @@ export const Navbar: FC = () => {
                 </>
               ) : (
                 <li className="menu__item">
+                  <div className="translate" />
                   <a href="/" className="menu__link">
                     HOME
                   </a>
